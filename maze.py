a57d792c8ceafc5593fc7fb8aecc5601c05cc924
Maria AFARA <maria-afara5@hotmail.com>
2022-12-12 03:00:26 +0100

    Add maze.py
    - Create a Maze made up of cells

---
+++ maze.py
@@ -0,0 +1,18 @@
+"""TODO: fill."""
+
+class Cell:
+    """A cell in the maze.
+
+    A maze "Cell" is a point in the grid which may be surrounded by walls in these directions:
+    up, down, left or right.
+    """
+    def __init__(self):
+        """Initialize the cell."""
+
+
+
+class Maze:
+    """A Maze, represented as a grid of cells."""
+
+    def __init__(self):
+        """Initialize the maze grid."""
