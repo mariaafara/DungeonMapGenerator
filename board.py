ac80086f62574b240aaf884c33b9508f7574268c
Maria AFARA <maria-afara5@hotmail.com>
2022-12-12 01:46:27 +0100

    Add Board and Qlearner modules
    - Add board.py which includes the Board module
    - Add q_learner.py which includes the QLearner module
    - ADD contants.py

---
+++ board.py
@@ -0,0 +1,89 @@
+"""This module TODO: fill."""
+import random
+from typing import Tuple
+
+from constants import END_CELL_VALUE, TREASURE_CELL_VALUE
+
+
+class Board(object):
+    """Our Game environment."""
+
+    def __init__(self, map_size: int):
+        """Constructor init."""
+        self.map_width = map_size
+        self.map_height = map_size
+        self.cell_values = {}  # Maps cell (x, y) to reward r
+        self.starting_point, self.ending_point, self.treasure_point = self.init_points()
+        self.init_cell_rewards()
+        self.is_treasure_reached = False
+        self.is_end_reached = False
+
+    def __generate_random_point(self, chosen_points: set = None) -> Tuple[int, int]:
+        """TODO: fill."""
+        while True:
+            point = (random.randint(0, self.map_width - 1), random.randint(0, self.map_height - 1))
+            if chosen_points is None or point not in chosen_points:
+                return point
+
+    def init_points(self) -> Tuple[Tuple[int, int], Tuple[int, int], Tuple[int, int]]:
+        """Initialize starting point, an ending point and a treasure point."""
+        starting_point = self.__generate_random_point()
+        treasure_point = self.__generate_random_point(chosen_points={starting_point})
+        ending_point = self.__generate_random_point(chosen_points={starting_point, treasure_point})
+        return starting_point, treasure_point, ending_point
+
+    def init_cell_rewards(self):
+        """Initialize the grid cell rewards, initially each cell reward is filled with 0."""
+        for x in range(self.map_width):
+            for y in range(self.map_height):
+                self.cell_values[(x, y)] = 0
+        self.fill_reward_cells()
+
+    def fill_reward_cells(self):
+        """Filee the treasure and end cell in the reward dictionary by their constant defined values."""
+        self.cell_values[self.treasure_point[0], self.treasure_point[1]] = TREASURE_CELL_VALUE
+        self.cell_values[self.ending_point[0], self.ending_point[1]] = END_CELL_VALUE
+
+    def is_terminal_cell(self, coord: Tuple[int, int]) -> bool:
+        """Determine if the specified location is a terminal (an end) state.
+
+        In our case, (reaching end_point after reaching the treasure_point) and (reaching treasure_point after
+        reaching the end_point) are the end states.
+        """
+        return (coord == self.ending_point and self.is_treasure_reached) or (
+                coord == self.treasure_point and self.is_end_reached)
+
+    def is_valid_cell(self, coord: Tuple[int, int], action: str) -> bool:
+        """Check if the next state after executing an action can still be a valid state.
+
+        A valid state is a state that stays within the boundaries of the grid.
+        """
+        x_coord, y_coord = self.get_cell_after_action(coord, action)
+        return 0 <= x_coord < self.map_width and 0 <= y_coord < self.map_height
+
+    def get_cell_after_action(self, coord: Tuple[int, int], action: str) -> Tuple[int, int]:
+        """Get the next cell coord after executing an action.
+
+        action: up (x, y+1), down (x, y-1), left (x-1, y), right (x+1, y)
+        """
+        x_coord, y_coord = coord
+        if action == 'left':
+            x_coord -= 1
+        elif action == 'right':
+            x_coord += 1
+        elif action == 'up':
+            y_coord += 1
+        elif action == 'down':
+            y_coord -= 1
+        return x_coord, y_coord
+
+    def get_cell_value(self, coord: Tuple[int, int]) -> int:
+        """Returns the value of a state( represented by its coord).
+
+        For example, Value(self.treasure_point) is 50 and Value(self.end_point) is 25 and the Value(cellOther) is 0.
+        """
+        return self.cell_values[coord]
+
+    def get_cells(self):
+        """TODO:fill."""
+        return self.cell_values.keys()
