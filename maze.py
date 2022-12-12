a8cd3a257d3df5321a0a598f71874dc525ee1164
Maria AFARA <maria-afara5@hotmail.com>
2022-12-12 03:06:01 +0100

    Cell: initialize cell with coord and build it's 4 direction walls

--- maze.py
+++ maze.py
@@ -6,9 +6,18 @@ class Cell:
     A maze "Cell" is a point in the grid which may be surrounded by walls in these directions:
     up, down, left or right.
     """
-    def __init__(self):
-        """Initialize the cell."""
+    # A wall separates a pair of cells in the up-down or left-right directions.
+    wall_pairs = {'up': 'down', 'down': 'up', 'right': 'left', 'left': 'right'}
 
+    def __init__(self, coord):
+        """Initialize the cell at coord = (x,y). At first, it is surrounded by walls."""
+        self.coord = coord
+        self.walls = {'up': True, 'down': True, 'right': True, 'left': True}
+
+    def __str__(self):
+        """TODO: fill."""
+        s = f"coord:{self.coord} -- walls:{self.walls}"
+        return s
 
 
 class Maze:
